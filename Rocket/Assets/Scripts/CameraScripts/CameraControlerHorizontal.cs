6abb5bb6ec2911022acd9398a5d44982fbe276d5
id-zonex <idzone027@gmail.com>
2021-01-17 01:41:56 +0200

    Rocket_2_3_bild_11_new_horisontal_move_camera

---
+++ Rocket/Assets/Scripts/CameraScripts/CameraControlerHorizontal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlerHorizontal : MonoBehaviour
+{
+    public Transform Player;
+
+    public float smoothTime = 0.2f;
+    Vector3 _velocity = Vector3.zero;
+    public Vector3 stopPocition;
+    public Vector3 stopPocition2;
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!(Player.position.x <= stopPocition.x || Player.position.x >= stopPocition2.x))
+        {
+            print("g");
+            Vector3 targetPosition = new Vector3(Player.position.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
+        }
+    }
+}
