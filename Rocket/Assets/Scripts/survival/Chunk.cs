66bf068edb39ef4c02402af40b5026f47442160f
id-zonex <idzone027@gmail.com>
2021-01-23 15:56:59 +0200

    Rocket v2_9 Update survival mode

---
+++ Rocket/Assets/Scripts/survival/Chunk.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public Transform EndPos;
+    public Transform MiddlePos;
+    public Transform StartPos;
+    public GameObject Wall;
+}
